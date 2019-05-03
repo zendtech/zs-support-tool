b500e79a08435cf6ea35f12c770a4a5f4ffd8053
Roman Basayev <roman.basayev@roguewave.com>
2019-05-03 14:24:52 +0200

    Update for Zend Server 2019

--- ZSST_IBMi_ZPK/create_ZPK.sh
+++ ZSST_IBMi_ZPK/create_ZPK.sh
@@ -2,6 +2,9 @@
 
 TS=$(date +%y%m%d.%H%M%S)
 
-sed -i '' "s|^\(.*<release>9\.1\.\).*\(</release>\)|\1$TS\2|" deployment.xml
+mv deployment.xml deployment.xml.BAK
+sed "s|^\(.*<release>2019\.0\.\).*\(</release>\)|\1$TS\2|" deployment.xml.BAK > deployment.xml
 
-zip -r -9 ZSST_IBMi_9.1.${TS}.zpk data scripts read.txt ZSST_icon.png deployment.xml
+zip -r -9 ZSST_IBMi_2019.0.${TS}.zpk data scripts read.txt ZSST_icon.png deployment.xml
+rm -f deployment.xml
+mv deployment.xml.BAK deployment.xml
