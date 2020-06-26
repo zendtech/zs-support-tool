e94256d7e4b78f55311500f35ace9df168dd8a57
Roman Basayev <roman.basayev@roguewave.com>
2020-06-26 12:58:04 +0200

    Unified shell code for IBMi - regular and ZPK

--- ZSST_IBMi_ZPK/create_ZPK.sh
+++ ZSST_IBMi_ZPK/create_ZPK.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-TS=$(date +%y%m%d.%H%M%S)
+PackDir=/tmp/zsst_zpk_pack_$(date +%s)
+TS=$(date +%y%m%d)
 
-mv deployment.xml deployment.xml.BAK
-sed "s|^\(.*<release>2018\.0\.\).*\(</release>\)|\1$TS\2|" deployment.xml.BAK > deployment.xml
+mkdir $PackDir
 
-zip -r -9 ZSST_IBMi_2018.0.${TS}.zpk data scripts read.txt ZSST_icon.png deployment.xml
-rm -f deployment.xml
-mv deployment.xml.BAK deployment.xml
+cp deployment.xml $PackDir/d.xml
+cp read.txt ZSST_icon.png $PackDir/
+cp -R data scripts $PackDir/
+cp ../ZSST_IBMi/bin/support_tool.sh $PackDir/data/bin/
+cp -R ../ZSST_IBMi/share $PackDir/data/
+
+pushd .
+cd $PackDir
+
+sed "s|^\(.*<release>One\.\).*\(</release>\)|\1$TS\2|" d.xml > deployment.xml
+zip -r -9 ZSST_IBMi_One.${TS}.zpk data scripts read.txt ZSST_icon.png deployment.xml
+
+popd
+mv $PackDir/ZSST_IBMi_One.${TS}.zpk .
+
+rm -rf $PackDir
